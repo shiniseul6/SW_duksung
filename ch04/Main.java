2b15c9dc423ee335ae1490add06a5566edcb0572
shiniseul6 <shinisxul@duksung.ac.kr>
2026-04-06 22:56:44 +0900

    Add files via upload

---
+++ ch04/Main.java
@@ -0,0 +1,25 @@
+package hw.ch04;
+
+import hw.ch04.framework.Factory;
+import hw.ch04.framework.Product;
+import hw.ch04.idcard.IDCardFactory;
+import hw.ch04.license.LicenseCardFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("학번: 20240873 | 이름: 신이슬");
+        
+        Factory factory1 = new IDCardFactory("2026-01-15");
+        Product card1 = factory1.create("Kim Youngjin");
+        Product card2 = factory1.create("Son Heungmin");
+        card1.use();
+        card2.use();
+
+        System.out.println(); 
+        Factory factory2 = new LicenseCardFactory("2026-03-20");
+        Product license1 = factory2.create("Park Jisung");
+        Product license2 = factory2.create("Lee Min-jae");
+        license1.use();
+        license2.use();
+    }
+}
