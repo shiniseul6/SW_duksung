88b604b2a3cacbbba5be2ce2885c9d7e498ca92e
shiniseul6 <shinisxul@duksung.ac.kr>
2026-05-26 00:32:35 +0900

    Update Main.java

--- ch19/Main.java
+++ ch19/Main.java
@@ -2,6 +2,7 @@ package hw.ch19;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번: 20240873 / 이름: 신이슬\n");
         SafeFrame frame = new SafeFrame("State Sample");
         while (true) {
             for (int hour = 0; hour < 24; hour++) {
